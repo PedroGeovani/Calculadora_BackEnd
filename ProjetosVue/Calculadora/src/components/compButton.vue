29b8712edff94dcbcdd5efd97de9071b1daf0086
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-18 10:52:59 -0300

    Componentes adicionados

---
+++ ProjetosVue/Calculadora/src/components/compButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>
+        <h3> {{title}} </h3>
+    </button>
+</template>
+
+<script>
+    export default{
+        name: 'button',
+        date(){
+            return{
+                title: "0"
+            }
+        }
+    }
+
+</script>
---
+++ ProjetosVue/Calculadora/src/components/compTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    
+        <h2> {{ text }} </h2>
+</template>
+
+<script>
+    export default{
+        name: 'title',
+        date(){
+            return{
+                text: "Calculadora"
+            }
+        }
+    }
+
+</script>
