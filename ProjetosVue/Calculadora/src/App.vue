0f2484f36e41f9b7e2710ccf2206d52533574039
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-18 10:52:59 -0300

    Componentes adicionados

--- ProjetosVue/Calculadora/src/App.vue
+++ ProjetosVue/Calculadora/src/App.vue
@@ -1,5 +1,28 @@
 <template>
 <div>
-  <
-  <h1></h1>
+  <compTitle/>
+  <compDisplay/>
+  <compButton/>
+
+</div>
+
 </template>
+
+<script>
+import compButton from "./components/compButton.vue"
+import compDisplay from "./components/compDisplay.vue"
+import compTitle from './components/compTitle.vue'
+import compTitle from "./components/compTitle.vue"
+
+export default{
+  components: { compTitle },
+  name: 'App',
+  date(){
+    return{
+      compTitle,
+      compDisplay,
+      compButton
+    }
+  }
+} 
+</script>
---
+++ ProjetosVue/Calculadora/src/components/compDisplay.vue
@@ -0,0 +1,15 @@
+<template>    
+    <h2> {{ text }} </h2>
+</template>
+
+<script>
+export default{
+    name: 'display',
+    date(){
+        return{
+            text: "Saida de comandos"
+        }
+    }
+}
+
+</script>
