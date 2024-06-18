80496b50e9b5a1eb3140b858cfec1c5e4e4ca484
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-06-18 13:59:40 -0300

    arquivo

--- ProjetosVue/Calculadora/src/App.vue
+++ ProjetosVue/Calculadora/src/App.vue
@@ -1,5 +1,52 @@
 <template>
-<div>
-  <
-  <h1></h1>
+  <div>
+    <h2> {{compTitleVue}}</h2>
+    <h3> {{ compDisplay }}</h3>
+    <div>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+    </div>
+
+    <div>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+    </div>
+
+    <div>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+    </div>
+
+    <div>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+      <h3> {{ compButton }}</h3>
+    </div>
+  </div>
+ 
 </template>
+
+<script>
+import compButtonVue from './components/compButton.vue'
+import compDisplayVue from './components/compDisplay.vue'
+import compTitleVue from './components/compTitle.vue'
+
+export default {
+  name: 'App',
+  date(){
+    return{
+      compTitleVue,
+      compDisplayVue,
+      compButtonVue,
+
+    }
+  }
+}
+</script>
